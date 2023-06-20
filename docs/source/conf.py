8780389e85cab997ea3d60e996839176666a32a5
Zhiyi Sun <zhiyisun@msn.com>
2023-06-21 05:43:42 +0800

    feat: Add section for OVS and driver uninstallation
    Signed-off-by: Zhiyi Sun <zhiyi.sun@amd.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,10 +28,10 @@ copyright = '2023, AMD, Inc.'
 author = 'AMD, Inc.'
 
 # The short X.Y version
-version = '1.6'
+version = '1.7'
 # The full version, including alpha/beta/rc tags
-release = '1.6'
-html_last_updated_fmt = 'Jan 12, 2023'
+release = '1.7'
+html_last_updated_fmt = 'June 21, 2023'
 
 # -- General configuration ---------------------------------------------------
 
